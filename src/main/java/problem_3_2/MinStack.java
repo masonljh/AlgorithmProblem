90f2d7876b1b17284c9b20359656d7f9e80deaf4
masonljh <dlwhdgy9190@gmail.com>
2019-02-11 11:23:55 +0900

    3_2 문제 주석 추가

--- src/main/java/problem_3_2/MinStack.java
+++ src/main/java/problem_3_2/MinStack.java
@@ -22,18 +22,23 @@ class MinStack {
 
     void push(int value) {
         if (currentIdx < 0) {
+            // 스택에 값이 없었다면 입력받은 값을 스택과 최소값 배열에 넣고 index를 증가시킴
             currentIdx++;
             arrays[currentIdx] = value;
             minArrays[currentIdx] = value;
             return;
         }
 
+        // 스택에 값이 있는 경우 마지막 값 비교
         if (minArrays[currentIdx] > value) {
+            // 새로 입력받은 값이 최소값인 경우
             minArrays[currentIdx + 1] = value;
         } else {
+            // 새로 입력받은 값이 최소값이 아닌 경우
             minArrays[currentIdx + 1] = minArrays[currentIdx];
         }
 
+        // 스택에 쌓고 인덱스 증가
         currentIdx++;
         arrays[currentIdx] = value;
     }
@@ -44,6 +49,7 @@ class MinStack {
     }
 
     int min() {
+        // 최소값은 최소값 배열의 현재 index로 리턴
         return minArrays[currentIdx];
     }
 }

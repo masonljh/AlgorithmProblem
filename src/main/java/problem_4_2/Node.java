8a81b8038060161bc8ef9bd75e37d52f6c77f660
masonljh <dlwhdgy9190@gmail.com>
2019-02-19 15:14:55 +0900

    안 쓰는 코드 지움

--- src/main/java/problem_4_2/Node.java
+++ src/main/java/problem_4_2/Node.java
@@ -1,51 +1,16 @@
 package problem_4_2;
 
 public class Node {
-    private int height;
     private int value;
-    private int maxValue;
-    private int minValue;
-    private Node root;
     private Node leftNode;
     private Node rightNode;
 
     public Node(int value) {
         this.value = value;
-        this.height = 1;
-        this.maxValue = Integer.MAX_VALUE;
-        this.minValue = Integer.MIN_VALUE;
     }
 
     public int getValue() {
         return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-    public int getMaxValue() {
-        return maxValue;
-    }
-
-    public void setMaxValue(int maxValue) {
-        this.maxValue = maxValue;
-    }
-
-    public int getMinValue() {
-        return minValue;
-    }
-
-    public void setMinValue(int minValue) {
-        this.minValue = minValue;
-    }
-
-    public Node getRoot() {
-        return root;
-    }
-
-    public void setRoot(Node root) {
-        this.root = root;
     }
 
     public Node getLeftNode() {
@@ -62,19 +27,5 @@ public class Node {
 
     public void setRightNode(Node rightNode) {
         this.rightNode = rightNode;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public void swap(Node other) {
-        int tempValue = this.value;
-        this.value = other.value;
-        other.value = tempValue;
     }
 }

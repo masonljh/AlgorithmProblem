3f4e300129196dae66db76818bd6db8d5892649c
masonljh <dlwhdgy9190@gmail.com>
2019-02-19 15:08:20 +0900

    4_2 README.md 업데이트 및 쓸모없는 파일 삭제

--- src/main/java/problem_4_2/WrongModeException.java
+++
@@ -1,7 +0,0 @@
-package problem_4_2;
-
-public class WrongModeException extends RuntimeException {
-    public WrongModeException() {
-        super("Current mode cannot suppot this command.");
-    }
-}

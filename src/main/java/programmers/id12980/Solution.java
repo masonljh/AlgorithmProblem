f133e65a324a3ce93a2b6f46b14a32c37618a253
masonljh <dlwhdgy9190@gmail.com>
2019-03-21 16:59:09 +0900

    풀이 조금더 바꿈

--- src/main/java/programmers/id12980/Solution.java
+++ src/main/java/programmers/id12980/Solution.java
@@ -2,16 +2,17 @@ package programmers.id12980;
 
 public class Solution {
     public int solution(int n) {
-        int ans = 0;
+        // 처음에 무조건 한 번은 점프 뛰어야함
+        int ans = 1;
 
         while(n >= 2) {
             if (n % 2 == 1) {
+                // 나머지가 있다면 배터리 1칸 사용(점프)
                 ans++;
             }
+            // 순간이동(거리는 반으로)
             n /= 2;
         }
-
-        ans++;
         return ans;
     }
 }
